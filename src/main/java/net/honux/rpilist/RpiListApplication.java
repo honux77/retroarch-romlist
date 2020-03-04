84dee0fa1cf98df55cee8f3cb9b4526afe2d7afe
Hoyoung Jung <honux77@gmail.com>
2020-03-04 16:08:17 +0900

    Init

---
+++ src/main/java/net/honux/rpilist/RpiListApplication.java
@@ -0,0 +1,13 @@
+package net.honux.rpilist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpiListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpiListApplication.class, args);
+	}
+
+}
